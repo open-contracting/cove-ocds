bfab55fd9d53941002b5de768dcdcb0756a9918a
Ben Webb <ben.webb@opendataservices.coop>
2020-08-06 14:15:17 +0000

    views: Fix gettext calls, so that strings are extracted correctly
    https://github.com/open-contracting/cove-ocds/pull/45#issuecomment-668611700

--- cove_ocds/views.py
+++ cove_ocds/views.py
@@ -78,9 +78,9 @@ def explore_ocds(request, pk):
                     'sub_title': _("Sorry, we can't process that data"),
                     'link': 'index',
                     'link_text': _('Try Again'),
-                    'msg': _(format_html("The file that you uploaded doesn't appear to be well formed JSON. OCDS JSON follows the I-JSON format, which requires UTF-8 encoding. Ensure that your file uses UTF-8 encoding, then try uploading again."
+                    'msg': format_html(_("The file that you uploaded doesn't appear to be well formed JSON. OCDS JSON follows the I-JSON format, which requires UTF-8 encoding. Ensure that your file uses UTF-8 encoding, then try uploading again."
                              '\n\n<span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true">'
-                             '</span> <strong>Error message:</strong> {}', err)),
+                             '</span> <strong>Error message:</strong> {}'), err),
                     'error': format(err)
                 })
             except ValueError as err:
@@ -89,13 +89,13 @@ def explore_ocds(request, pk):
                         "sub_title": _("Sorry, we can't process that data"),
                         "link": "index",
                         "link_text": _("Try Again"),
-                        "msg": _(
-                            format_html(
+                        "msg": format_html(
+                            _(
                                 "We think you tried to upload a JSON file, but it is not well formed JSON."
                                 '\n\n<span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true">'
                                 "</span> <strong>Error message:</strong> {}",
-                                err,
-                            )
+                            ),
+                            err,
                         ),
                         "error": format(err),
                     }
