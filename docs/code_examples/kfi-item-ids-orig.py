818dd9b73ce4a8331d0221a77f2e942617b2524e
James McKinney <26463+jpmckinney@users.noreply.github.com>
2021-05-27 17:54:52 -0400

    docs: Lint code examples

--- docs/code_examples/kfi-item-ids-orig.py
+++ docs/code_examples/kfi-item-ids-orig.py
@@ -1,6 +1,6 @@
 tender_items = tender.get('items', [])
 for item in tender_items:
-	item_id = item.get('id')
-	if item_id and release_id:
-		release_tender_item_ids.add((ocid, release_id, item_id))
-		get_item_scheme(item)
+    item_id = item.get('id')
+    if item_id and release_id:
+        release_tender_item_ids.add((ocid, release_id, item_id))
+        get_item_scheme(item)
