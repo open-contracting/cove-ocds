5600dbafdc54c2a2383bf0d6f0b61aecc8fde441
James McKinney <26463+jpmckinney@users.noreply.github.com>
2021-11-10 10:18:30 -0500

    urls: Use settings.URL_PREFIX, and path instead of re_path

--- cove_project/urls.py
+++ cove_project/urls.py
@@ -2,17 +2,16 @@ from cove.urls import handler500  # noqa: F401
 from cove.urls import urlpatterns as urlpatterns_core
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import include, re_path
+from django.urls import include, path, re_path
 from django.views.generic import RedirectView
 
 import cove_ocds.views
 
-# Serve the OCDS validator at /validator/
 urlpatterns_core += [re_path(r"^data/(.+)$", cove_ocds.views.explore_ocds, name="explore")]
 
 urlpatterns = [
-    re_path(r"^$", RedirectView.as_view(url="review/", permanent=False)),
-    re_path("^review/", include(urlpatterns_core)),
+    path("", RedirectView.as_view(pattern_name="index", permanent=False)),
+    path(settings.URL_PREFIX, include(urlpatterns_core)),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
